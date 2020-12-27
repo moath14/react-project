a93cd799b0badc70291a8457beefb17721160592
moath14 <moaalnajjar@gmail.com>
2020-12-27 23:43:07 +0200

    Updates

---
+++ precache-manifest.199dc77437575dd84eac07377af20afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb24a0fac77e1870059c57665121458c",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "6d57737c0d4a6c7f4b2a",
+    "url": "/react-project/static/css/main.ec8dda66.chunk.css"
+  },
+  {
+    "revision": "5914ecc8e81388a16b02",
+    "url": "/react-project/static/js/2.39fffdb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.39fffdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d57737c0d4a6c7f4b2a",
+    "url": "/react-project/static/js/main.b541357c.chunk.js"
+  },
+  {
+    "revision": "5b6fe30e47331518a2e2",
+    "url": "/react-project/static/js/runtime-main.fc2d02f5.js"
+  }
+]);
